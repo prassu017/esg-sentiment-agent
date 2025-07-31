c8ed1246824b46391cd8629a337f4790afb577a3
prassu017 <prassu017@gmail.com>
2025-07-31 04:38:07 -0700

    Changed level exec

--- src/ingestion.py
+++ src/ingestion.py
@@ -1,5 +1,8 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import streamlit as st
 import pandas as pd
-import requests
 import time
 import yfinance as yf
 from datetime import datetime, timedelta
